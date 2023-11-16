0cdd711d37d3b080904eb7e4d8cea0647a63c781
Jaapkoster2004 <135604304+Jaapkoster2004@users.noreply.github.com>
2023-11-16 11:39:50 +0100

    edited smallangle so the number of values can be given using click

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,22 +1,48 @@
+# Small angle
+# Jaap Koster
+
+# import libraries
 import click
 import numpy as np
 from numpy import pi
 import pandas as pd
 
+#define the group of commands
+@click.group()
+def small_angle():
+    pass
 
+
+@small_angle.command()
+@click.option(
+    "-n",
+    "--number",
+    default = 10,
+    help = "The amount of values you want to calculate between 0 and 2 pi",
+    show_default = True
+)
+# calculates the sinus of x for N values of x between 0 and 2pi
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@small_angle.command()
+@click.option(
+    "-n",
+    "--number",
+    default = 10,
+    help = "The amount of values you want to calculate between 0 and 2 pi",
+    show_default = True
+)
+# calculates the tangens of x for N values of x between 0 and 2pi
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
-
+#makes it so small angle can be called from anywhere
 if __name__ == "__main__":
-    sin(10)
+    small_angle()
